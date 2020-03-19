9962e50c66316c56d26ab34a38a24d10ea9f3c61
mssc89 <oweziz89@gmail.com>
2020-03-19 18:47:31 +0100

    paths fix

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,16 @@ const postController = require('./postController');
 const authController = require('./authController');
 
 //posts
-router.get('posts', postController.index);
-router.post('posts', auth, postController.new);
+router.get('/posts', postController.index);
+router.post('/posts', auth, postController.new);
 
 //single post
-router.get('posts/:post_id', postController.view);
-router.put('posts/:post_id', auth, postController.update);
-router.delete('posts/:post_id', auth, postController.delete);
+router.get('/posts/:post_id', postController.view);
+router.put('/posts/:post_id', auth, postController.update);
+router.delete('/posts/:post_id', auth, postController.delete);
 
 //auth
-router.post('auth/login', authController.login);
-router.post('auth/register', authController.register);
+router.post('/auth/login', authController.login);
+router.post('/auth/register', authController.register);
 
 module.exports = router;
